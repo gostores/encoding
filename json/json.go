e42805b59b8483e741399da345985792b2d13f54
Billgo <cocobill@vip.qq.com>
2017-11-30 17:34:03 +0800

    add json

---
+++ json/json.go
@@ -0,0 +1,11 @@
+// +build !jsoniter
+
+package json
+
+import "encoding/json"
+
+var (
+	Marshal       = json.Marshal
+	MarshalIndent = json.MarshalIndent
+	NewDecoder    = json.NewDecoder
+)
---
+++ json/jsoniter.go
@@ -0,0 +1,12 @@
+// +build jsoniter
+
+package json
+
+import "github.com/json-iterator/go"
+
+var (
+	json          = jsoniter.ConfigCompatibleWithStandardLibrary
+	Marshal       = json.Marshal
+	MarshalIndent = json.MarshalIndent
+	NewDecoder    = json.NewDecoder
+)
